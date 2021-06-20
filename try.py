93a4903a4019059964f5d3bd6ff9778d74826684
Diana Aguilar <dianaleticia2001@hotmail.com>
2021-06-20 00:22:23 -0500

    First articulations

--- try.py
+++ try.py
@@ -11,16 +11,20 @@ mp_drawing=mp.solutions.drawing_utils
 mp_holistic=mp.solutions.holistic
 
 capture =cv.VideoCapture(0)
-with mp_holistic.Holistic(min_detection_confidence=0.5,min_tracking_confidence=0.5)as holistic:
+with mp_holistic.Holistic(min_detection_confidence=0.8,min_tracking_confidence=0.8)as holistic:
      while capture.isOpened():
         
         data,frame = capture.read()
-        image= cv.cvtColor(frame,cv.COLOR_BGR2RGB)
+        image= cv.cvtColor(frame,cv.COLOR_RGB2BGR)
         result=holistic.process(image)
         
-        print(result)
+        print(result.pose_landmarks)
         
-        cv.imshow('camera',frame)
+        image= cv.cvtColor(image,cv.COLOR_BGR2RGB)
+        
+        mp_drawing.draw_landmarks(image, result.pose_landmarks,mp_holistic.POSE_CONNECTIONS)
+        
+        cv.imshow('camera',image)
         if cv.waitKey(2) == ord('q'):
             break   
 
